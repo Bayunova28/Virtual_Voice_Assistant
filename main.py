50db66d14b109596c5fc58764e8efd0d1e489577
Willibrordus Bayu <willibayu28@gmail.com>
2022-01-18 13:45:27 +0700

    Ace

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #import library
+from async_timeout import sys
 import speech_recognition as sr 
 import playsound  
 from gtts import gTTS  
@@ -90,8 +91,8 @@ engine = pyttsx3.init()
 
 #define function to response the audio
 def audio_response(voice_db):
-    if audio_exists(['hello Ace']):
-        audio_speak('hello, can i help you Sir?')
+    if audio_exists(['tell me your name']):
+        audio_speak('hello, my name is ace. Can I help you, Sir?')
 
     if audio_exists(['Ace what time is it']):
         time = datetime.datetime.now().strftime('%I:%M %p')
@@ -120,7 +121,7 @@ def audio_response(voice_db):
         url = 'https://search.kompas.com/search/?q=' + search
         webbrowser.get().open(url)
         audio_speak('Hello Sir, Here is what I found for ' + search + 'on kompas news!')
-
+    
     if audio_exists(['Ace search for']):
         search = voice_db.split('for')[-1]
         url = 'http://www.google.com/search?q=' + search
