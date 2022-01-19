1d7d3ca96b516d32ab617f14f36678c10e6da1d7
Willibrordus Bayu <willibayu28@gmail.com>
2022-01-19 13:31:15 +0700

    Ace

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import weathercom
 import json
 import pyttsx3
 import smtplib
+import requests
 
 #define function class person
 class person:
@@ -35,14 +36,6 @@ def audio_exists(terms):
         if term in voice_db:
             return True
 
-#define function to present the weather
-def audio_weather(city):
-    weather = weathercom.getCityWeatherDetails(city)
-    humidity = json.loads(weather)['vt1observation']['humidity']
-    temp = json.loads(weather)['vt1observation']['temperature']
-    phrase = json.loads(weather)['vt1observation']['phrase']
-    return humidity, temp, phrase
-
 #define function engine audio speak
 def audio_speak(text):
     txt = str(text)
@@ -63,11 +56,11 @@ def audio_record(ask = False):
 
         try:
             voice_db = recognition.recognize_google(audio_listen)
+
         except sr.UnknownValueError:
             audio_speak('I am sorry Sir, I did not understand what you said. Can you please repeat again!')
         except sr.RequestError:
             audio_speak('I am sorry Sir, my server is going down')
-
         print(voice_db)
         return voice_db
 
@@ -88,9 +81,20 @@ def audio_email(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('youremail@gmail.com', 'your-password')
-    server.sendmail('youremail@gmail.com', to, content)
+    server.login('willibayu28@gmail.com', 'BayuNova281120')
+    server.sendmail('willibayu28@gmail.com', to, content)
     server.close()
+
+#define function to start the conversation
+def audio_greet():
+    hour = datetime.datetime.now().hour
+    if hour >= 6 and hour < 12:
+        audio_speak('Good morning, Sir!')
+    elif hour >= 12 and hour < 18:
+        audio_speak('Good afternoon, Sir!')
+    elif hour >= 19 and hour < 24:
+        audio_speak('Good evening, Sir!')
+    audio_speak('Ace at your service. Please tell me how can i help you, Sir?')
 
 #generate function class person and assistant
 personObj = person()
@@ -100,20 +104,29 @@ engine = pyttsx3.init()
 
 #define function to response the audio
 def audio_response(voice_db):
-    if audio_exists(['tell me your name']):
-        audio_speak('hello my name is ace. Can I help you Sir?')
+    if audio_exists(['Ace search weather for']):
+        api_key = 'your-api-key'
+        base_url = 'https://api.openweathermap.org/data/2.5/weather?'
+        audio_speak('Where is the city?')
+        city_name = audio_record()
+        complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+        response = requests.get(complete_url)
+        x = response.json()
 
-    elif audio_exists(['Ace what time is it']):
+        if x['cod'] != '404':
+            y = x['main']
+            temp = round(y['temp'] - 273)
+            humidity = y['humidity']
+            z = x['weather']
+            description = z[0]['description']
+            audio_speak('Currently in ' + city_name + ' temperature is ' + str(temp) + ' degrees celcius' + '\n humidity in percentage is ' + 
+                        str(humidity) + ' percent' + '\n the condition is ' + str(description))
+            print('Currently in ' + city_name + ' temperature is ' + str(temp) + ' degrees celcius' + '\n humidity in percentage is ' + 
+                  str(humidity) + ' percent' + '\n the condition is ' + str(description))
+
+    if audio_exists(['Ace what time is it']):
         time = datetime.datetime.now().strftime('%I:%M %p')
         audio_speak('Current time is ' + time)
-
-    elif audio_exists(['Ace search weather for']):
-        city = audio_record('which city')
-        humidity, temp, phrase = audio_weather(city)
-        audio_speak("currently in " + city + "  temperature is " + str(temp) + " degree celsius, " + "humidity is " + str(humidity) + 
-                    " percent and sky is " + phrase)
-        print("currently in " + city + "  temperature is " + str(temp) + "degree celsius, " + "humidity is " + str(humidity) + " percent and sky is " 
-              + phrase)
     
     elif audio_exists(['Ace show my system']):
         system_path = "C:\Program Files (x86)\MSI\Dragon Center\Dragon Center.exe"
@@ -124,7 +137,7 @@ def audio_response(voice_db):
         try:
             audio_speak("What should I say? Sir")
             content = audio_record()
-            to = 'youremail@gmail.com'    
+            to = 'willibayu28@gmail.com'    
             audio_email(to, content)
             audio_speak('Email has been sent Sir')
         except Exception as e:
@@ -137,6 +150,12 @@ def audio_response(voice_db):
         print(songs)    
         audio_speak('Yes Sir! please wait')
         os.startfile(os.path.join(music_dir, songs[0]))
+    
+    elif audio_exists(['Ace where is']):
+        data = voice_db.split('for')[-1]
+        location_url = "https://www.google.com/maps/place/" + str(data[2])
+        webbrowser.get().open(location_url)
+        audio_speak("Hold on Dante, I will show you where " + data[2] + " is.")
 
     elif audio_exists(['Ace open Discord']):
         discord = "C:\\Users\\bayu\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Discord Inc\\Discord.lnk"
@@ -162,6 +181,9 @@ def audio_response(voice_db):
     elif audio_exists(['thank you']):
         audio_speak('you are welcome Sir. See you later!')
         sys.exit(0)
+
+#generate function start the conversation
+audio_greet()
 
 #define function to record the audio
 while (1):
